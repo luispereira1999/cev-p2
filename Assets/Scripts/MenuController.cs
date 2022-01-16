de675af8b7e42a4779f4acc80cea85898c966f82
Luís Pereira <luispereira1999@gmail.com>
2022-01-16 17:44:35 +0000

    Menu finalizado

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,14 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public string sceneName;
+    public Button buttonVolume;
+    public Sprite imageSoundOn;
+    public Sprite imageSoundOff;
 
     public void PlayGame(string sceneName)
     {
-        SceneManager.LoadScene(sceneName); 
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ViewInfo(GameObject panel)
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void CloseInfo(GameObject panel)
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    public void TriggerSound()
+    {
+        bool sound = AudioListener.pause;
+
+        if (sound == true)
+        {
+            buttonVolume.image.sprite = imageSoundOn;
+            ActivateSound();
+        }
+        else
+        {
+            buttonVolume.image.sprite = imageSoundOff;
+            DesactivateSound();
+        }
+    }
+
+    private void ActivateSound()
+    {
+        AudioListener.pause = false;
+    }
+
+    private void DesactivateSound()
+    {
+        AudioListener.pause = true;
     }
 }
